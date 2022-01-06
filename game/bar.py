989198b6f6de928335d4f1a6fef6e76340b5ac70
emelinevanderbeken <emelinevanderbeken@gmail.com>
2022-01-06 14:26:41 +0100

    Modified method to move bar

--- game/bar.py
+++ game/bar.py
@@ -16,12 +16,12 @@ class Paddle:
         self.bar = self.can.create_rectangle(self.x, self.y, self.x + self.width, self.y + self.height,
                                              fill='white', outline='white')
 
-    def move_right(self, event=None,  shift=0.75):
+    def move_right(self, event=None,  shift=1):
         x1, _, x2, _ = self.can.coords(self.bar)
         if x2 + 10 < self.can.winfo_width():
             self.can.move(self.bar, shift, 0)
 
-    def move_left(self, event=None, shift=-0.75):
+    def move_left(self, event=None, shift=-1):
         x1, _, x2, _ = self.can.coords(self.bar)
         if x1 - 10 > 0:
             self.can.move(self.bar, shift, 0)
